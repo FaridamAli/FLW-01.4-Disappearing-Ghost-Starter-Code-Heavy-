2224fc3c2446cff0bb07e8047344a1bea19e2cc5
FaridamAli <104842126+FaridamAli@users.noreply.github.com>
2022-11-10 23:11:33 +0000

    ghost

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+//all selector variables
+let p = document.createElement("p");
+let h1 = document.querySelector(".header");
+let input = document.querySelector(".input");
+let userInput;
+let ghostImg = document.querySelector(".ghost-image");
+let hideButton = document.querySelector(".hide-button");
+let showButton = document.querySelector(".show-button");
+let updateImg = document.querySelector(".update-img-button");
+let messageButton = document.querySelector(".message-button");
+let message = document.querySelector(".message");
+let nameButton = document.querySelector(".name-button");
 
+
+//create an onclick event for the Hide Me button
+hideButton.onclick = (function (){  
+ ghostImg.style.display="none"   
+});
+
+//create an onclick event for the Show Me button
+showButton.onclick = (function(){
+  ghostImg.style.display="initial"  
+});
+
+//create an onclick event for the Update Img button
+updateImg.onclick = (function(){
+ghostImg.src="https://i.etsystatic.com/21833184/r/il/f6dc00/2462058844/il_570xN.2462058844_6d3w.jpg"
+});
+
+//create an onclick event for the Send Message button
+//update the userInput variable by saving the value of the input
+//use the insertAdjacentHTML method to append the userInput in a <p> tag
+messageButton.onclick = (function(){
+  userInput= document.querySelector(".input")
+  message.insertAdjacentHTML('beforeEnd',userInput.value);
+});
+
+
+//create an onclick event for the Name Me button
+//update the userInput variable by saving the value of the input
+//use the innerHTML method to update the h1 tag
+nameButton.onclick = (function(){
+    userInput= document.querySelector(".input")
+  h1.innerHTML = `<p> ${userInput.value}!</p>`;
+});
